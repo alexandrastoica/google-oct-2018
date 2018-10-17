747b7e0494ef77cab2e5d67676d0ccc9a27cce33
Helen Root <helenroot1@gmail.com>
2018-10-17 18:49:02 +0100

    Kat's additions

--- projects/codebar/src/backend/backend.py
+++ projects/codebar/src/backend/backend.py
@@ -20,7 +20,7 @@ def get_data():
 	attendingDF = participantsDF[participantsDF.signed == 0]
 	coachCount = len(attendingDF[participantsDF.coach_flag == 1])
 	participantCount = len(attendingDF[participantsDF.coach_flag == 0])
-	return data
+	return participantsDF
 
 app = Flask(__name__)
 
@@ -30,17 +30,50 @@ app = Flask(__name__)
 	# dataFrame = pd.DataFrame(data)
 	# output = dataFrame.groupBy('language')
     # return data
-    
+
 def group():
-	data = get_data()
-	participantsDF = pd.DataFrame(data)
+	participantsDF = get_data()
+	attendingDF = participantsDF[participantsDF.signed == 0]
+	participants = participantsDF[attendingDF.coach_flag == 0]
+	participants = participants.sort_values(['language'])
 	
+	def rolling_count(val):
+		if val == rolling_count.previous:
+			rolling_count.count += 1
+		else:
+			rolling_count.previous = val
+			rolling_count.count = 1
+
+		return rolling_count.count
+
+	rolling_count.count = 0 
+	rolling_count.previous = None
+
+	def make_small(val):
+		if val % 2 == 0:
+			val -= 1
+		else:
+			val
+		return val
+
+	make_small.count = 0
+	make_small.previous = None
+
+	participants['testy'] = participants['language'].apply(rolling_count)
+
+	participants['group'] = participants['testy'].apply(make_small)
+	participants = participants[['name','language','group']]
+
 	print(participantsDF.name.unique())
 	# output = group_students(data)
 	# output.coachCount = 0
 	# output.studentCount = 0
 	# output.groupCount = [[4,3], [2,3], [5,1]]
 	# return jsonify(output)
-	return 'Hello, World'
+	df_list = participants.values.tolist()
+	return jsonify(df_list)
+
+
 
 # group()
+
